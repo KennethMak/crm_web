cec03178fa594cdea4db6f81682a0d115894b340
Kenneth Mak <kennethmak@Kenneths-MacBook-Pro.local>
2014-08-19 19:23:45 -0400

    finished

--- crms.rb
+++ crms.rb
@@ -9,9 +9,21 @@ get '/' do
 	erb :index
 end
 
+get "/contacts" do
+	erb :contacts
+end
+
 get "/contacts/new" do
 	erb :new_contact
 end
+
+post '/contacts' do
+	new_contact = Contact.new(params[:first_name], params[:last_name], params[:email], params[:note])
+	@@rolodex.add_contact(new_contact)
+	redirect to('/contacts')
+end
+
+
 # get "/contacts" do
 # 	@contacts = []
 # 	@contacts << Contact.new("Mr", "T", "mrt@live.ca", "tman")
