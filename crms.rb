fb3588c092ae87e5ee91f5122836dd66f9a6a8d9
Kenneth Mak <kennethmak@Kenneths-MacBook-Pro.local>
2014-08-19 18:04:57 -0400

    cmon

--- crms.rb
+++ crms.rb
@@ -5,3 +5,12 @@ get '/' do
 	@crm_app_name = "see our em"
 	erb :index
 end
+
+get "/contacts" do
+	@contacts = []
+	@contacts << Contact.new("Mr", "T", "mrt@live.ca", "tman")
+	@contacts << Contact.new("Randy", "Orton", "randyorton@live.ca", "exuviax")
+	@contacts << Contact.new("Fire", "Man", "fireman@live.ca", "wayne")
+
+	erb :contacts
+end
