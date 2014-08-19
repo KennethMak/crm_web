2278a5cec04a95191e0ca78be9f4a0e8d2b0ee04
Kenneth Mak <kennethmak@Kenneths-MacBook-Pro.local>
2014-08-19 18:22:02 -0400

    wordd

--- crms.rb
+++ crms.rb
@@ -2,16 +2,21 @@ require_relative'rolodex'
 require_relative 'contact'
 require 'sinatra'
 
+@@rolodex = Rolodex.new
+
 get '/' do
 	@crm_app_name = "see our em"
 	erb :index
 end
 
-get "/contacts" do
-	@contacts = []
-	@contacts << Contact.new("Mr", "T", "mrt@live.ca", "tman")
-	@contacts << Contact.new("Randy", "Orton", "randyorton@live.ca", "exuviax")
-	@contacts << Contact.new("Fire", "Man", "fireman@live.ca", "wayne")
-
-	erb :contacts
+get "/contacts/new" do
+	erb :new_contact
 end
+# get "/contacts" do
+# 	@contacts = []
+# 	@contacts << Contact.new("Mr", "T", "mrt@live.ca", "tman")
+# 	@contacts << Contact.new("Randy", "Orton", "randyorton@live.ca", "exuviax")
+# 	@contacts << Contact.new("Fire", "Man", "fireman@live.ca", "wayne")
+
+# 	erb :contacts
+# end
